d674cc17cbffde9b424cf5fb10afa4c74680def0
Zoe Railing <zrailing@iu.edu>
2019-02-17 17:49:18 -0500

    Added CREATE & FK to Replies

---
+++ app/sql/replies.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Replies (
+reply_id         INT(8) NOT NULL AUTO_INCREMENT,
+reply_content        TEXT NOT NULL,
+reply_date       DATETIME NOT NULL,
+reply_case     INT(8) NOT NULL,
+reply_thread   INT(8) NOT NULL,
+reply_by     INT(8) NOT NULL,
+PRIMARY KEY (reply_id),
+FOREIGN KEY (reply_case) REFERENCES cases(id) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY (reply_thread) REFERENCES Threads(thread_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+FOREIGN KEY (reply_by) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=INNODB;
