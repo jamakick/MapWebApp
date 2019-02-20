ca37e3f53200bb90d62b188d09c422baf11531dd
Zoe Railing <zrailing@iu.edu>
2019-02-20 08:24:47 -0500

    Successfully adds thread to Threads table

--- app/createThreadProcess.php
+++ app/createThreadProcess.php
@@ -2,15 +2,15 @@
 //createThreadProcess.php
 session_start();
 
-if(!$_SESSION["username"]) /* FIND BETTER WAY TO ANNOTATE SIGNED-ON-NESS. if($_SESSION['signed_in'] == false) */
+if(!$_SESSION["username"]) /* FIND BETTER WAY TO ANNOTATE SIGNED-ON-NESS, e.g. if($_SESSION['signed_in'] == false) */
 {
     //the user is not signed in
-    echo 'Sorry, you have to be <a href="/forum/signin.php">signed in</a> to create a topic.';
+    echo "Sorry, you have to be <a href='../users/login.php'>signed in</a> to create a topic.";
 }
 else
 {
 
-    $connection=mysqli_connect("db.soic.indiana.edu", "i494f18_team38", "my+sql=i494f18_team38", "i494f18_team38");
+    $connection= mysqli_connect("db.soic.indiana.edu", "i494f18_team38", "my+sql=i494f18_team38", "i494f18_team38"); // this info can be consolidated
 
     if (!$connection) {
       die("Failed to connect to MySQL: " . mysqli_connect_error() );
@@ -20,49 +20,76 @@ else
     $username = $_SESSION["username"];
 
 
-    //start the transaction
-    $query  = "BEGIN WORK;";
-    $result = mysql_query($query);
 
-    if(!$result)
+    //start the transaction -- abundance of caution
+    $query  = "BEGIN WORK;";
+    $result_bw = mysqli_query($connection, $query);
+
+    if(!$result_bw)
     {
-        //Damn! the query failed, quit
-        echo 'An error occured while creating your thread. Please try again later.';
+        //the query failed, quit
+        echo "An error occured while creating your thread." . mysqli_error($connection) . " Please try again later.";
     }
     else
     {
+        mysqli_free_result($query);
 
-        //the form has been posted, so save it
-        //insert the topic into the topics table first, then we'll save the post into the posts table
-        $sql = "INSERT INTO
-                    topics(topic_subject,
-                           topic_date,
-                           topic_cat,
-                           topic_by)
-               VALUES('" . mysql_real_escape_string($_POST['topic_subject']) . "',
-                           NOW(),
-                           " . mysql_real_escape_string($_POST['topic_cat']) . ",
-                           " . $_SESSION['user_id'] . "
-                           )";
 
-        $result = mysql_query($sql);
+
+        //lookup user ID based on username
+        $user_lookup = "SELECT user_id FROM users WHERE username = '$username'; ";
+      //  echo $user_lookup;
+        $user_result = mysqli_query($connection, $user_lookup);
+
+        if (!$user_result)
+        {
+          //something went wrong, display the date_get_last_errors
+          echo "An error occurred when confirming your account. Please try again later. This is the error:" . mysqli_error($connection);
+        }
+
+        else
+        {
+
+          while ($record = mysqli_fetch_assoc($user_result)) {
+            $user_id = $record["user_id"];
+          }
+
+        }
+
+
+
+        //create needed variables given by user
+        $content = mysqli_real_escape_string($connection, $_POST["content"]);
+        $title = mysqli_real_escape_string($connection, $_POST["title"]);
+        //create needed variables that are not submitted by the users
+        $case_id = mysqli_real_escape_string($connection, 13); //hard coded until we can work out how we want to handle detecting case
+
+        date_default_timezone_get(); //set time to the zone the server is on
+        $date_time = mysqli_real_escape_string($connection, date("Y-m-d H:i:s"));
+
+
+        //insert the thread
+        $sql = "INSERT INTO Threads(thread_content, thread_title, thread_case, thread_date, thread_by) VALUES ('$content', '$title', $case_id, '$date_time', '$user_id');";
+      //  echo $sql;
+
+        $result = mysqli_query($connection, $sql);
         if(!$result)
         {
             //something went wrong, display the error
-            echo 'An error occured while inserting your data. Please try again later.' . mysql_error();
+            echo 'An error occured while inserting your data. Please try again later. This is the error:' . mysqli_error($connection);
             $sql = "ROLLBACK;";
-            $result = mysql_query($sql);
+            $result = mysqli_query($connection, $sql);
         }
 
         else
         {
             $sql = "COMMIT;";
-            $result = mysql_query($sql);
+            $result = mysqli_query($connection, $sql);
 
-            //after a lot of work, the query succeeded!
-            echo 'You have successfully created <a href="topic.php?id='. $topicid . '">your new topic</a>.';
+            //the query succeeded!
+            echo 'You have successfully created your new thread. Check out the <a href="forum.php">discussion</a>.'; // Find way to redirect, e.g. '<a href="thread.php?id='. $thread_id . '">your new thread</a>.';
         }
-      }
+    }
 }
 
 ?>
