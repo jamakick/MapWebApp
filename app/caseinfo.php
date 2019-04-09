30d1d6c9d2921a55887166e326778d3f211abd9c
Zoe Railing <zrailing@iu.edu>
2019-04-09 15:27:28 -0400

    Fixed links to css

--- app/caseinfo.php
+++ app/caseinfo.php
@@ -7,9 +7,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<!-- Stylesheets -->
-		<link rel="stylesheet" href="../css/normalize.css">
-		<link rel="stylesheet" href="../css/styles.css">
-		<link rel="stylesheet" href="../css/styles2.css">
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/styles.css">
 
 		<!--[if lte IE 9]>
 			<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
